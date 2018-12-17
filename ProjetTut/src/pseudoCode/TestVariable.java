664f9b969dd3b4dcb229f8e1a14b6e47dd9b995a
Loan <loan.alouache@etu.univ-lehavre.fr>
2018-12-17 12:32:38 +0100

    a

--- ProjetTut/src/pseudoCode/TestVariable.java
+++ ProjetTut/src/pseudoCode/TestVariable.java
@@ -1,13 +1,27 @@
 package pseudoCode;
 
+import bsh.EvalError;
+import bsh.Interpreter;
+
 public class TestVariable {
 	
 	public static void main(String[] args) {
 		
-		String current = "si x < 2 alors lol";
-		String condition = current.split("si | alors")[0];
+		Interpreter i = new Interpreter();
 		
-		System.out.println(condition);
+		try {
+			String current = "si x < 2 alors lol";
+			String condition = current.split("si | alors")[1];
+			
+			System.out.println(condition);
+			i.eval("x = 1");
+			System.out.println(i.eval(condition));
+		} catch (EvalError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		
 		
