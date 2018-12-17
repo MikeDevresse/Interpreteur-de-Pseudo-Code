96ebf3eb22bb6446260f9d00db50f7056d39df4d
mikize <mike.devresse@gmail.com>
2018-12-17 08:26:23 +0100

    Lecture du fichier

--- ProjetTut/src/LectureFichier.java
+++ ProjetTut/src/LectureFichier.java
@@ -1,9 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class LectureFichier
 {
+	private BufferedReader fichier;
 	
 	public LectureFichier ( String fichier )
 	{
-		
+		try
+		{
+			this.fichier = new BufferedReader( new FileReader( fichier ) );
+				
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public String[] getTexteParLigne()
+	{
+		return this.toString().split( "\n" );
+	}
+	
+	public String toString ()
+	{
+		String sRet = "";
+		String s;
+		try
+		{
+    		while ( ( s = fichier.readLine() ) != null )
+    		{
+    			sRet += s;
+    		}
+		}
+		catch ( Exception e )
+		{
+			sRet += e;
+		}
+		return sRet;
 	}
 }
--- ProjetTut/src/pseudoCode/Variable.java
+++ ProjetTut/src/pseudoCode/Variable.java
@@ -2,6 +2,23 @@ package pseudoCode;
 
 public class Variable
 {
-	private String content;
+	private String contenu;
+	private String type;
+
+	public Variable ( String type, String contenu )
+	{
+		this.type = type;
+		this.contenu = contenu;
+	}
+
+	public Variable ( String type )
+	{
+		this( type, "" );
+	}
+
+	public void setContent ( String contenu )
+	{
+		this.contenu = contenu;
+	}
 
 }
