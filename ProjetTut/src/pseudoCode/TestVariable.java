40e9d7f3f47571a918ceeff4d8320681057126cf
Loan <alouache.loan@gmail.com>
2018-12-17 08:57:51 +0100

    détection des tableaux

--- ProjetTut/src/pseudoCode/TestVariable.java
+++ ProjetTut/src/pseudoCode/TestVariable.java
@@ -4,11 +4,13 @@ public class TestVariable {
 	
 	public static void main(String[] args) {
 		
-		String expression = "n <-- chaîne de caractère";
+//		String expression = "n <-- chaîne de caractère";
+//		
+//		Variable v = VariableFactory.createVariable(expression);
+//		
+//		System.out.println(v);
 		
-		Variable v = VariableFactory.createVariable(expression);
-		
-		System.out.println(v);
+		System.out.println(VariableFactory.createVariable("n <-- tableau d'entier"));
 		
 	}
 
--- ProjetTut/src/pseudoCode/VariableFactory.java
+++ ProjetTut/src/pseudoCode/VariableFactory.java
@@ -5,13 +5,40 @@ public class VariableFactory {
 	public static Variable createVariable(String expression) {
 		expression = expression.replace(" <", "<");
 		expression = expression.replace("- ", "-");
+		expression = expression.replace("d'", "de ");
 
 		String varName = expression.split("<--")[0];
 		String varType = expression.split("<--")[1];
 		varType = varType.toLowerCase();
-		
 
+		
+		//tableau de variables
+		String tabDelimiter = "tableau de ";
+		if (varType.indexOf(tabDelimiter) != -1) {
+			String tabType = varType.substring(varType.indexOf(tabDelimiter) + tabDelimiter.length());
+			
+			switch (tabType) {
+
+			case "entier":
+				return new Variable<Integer[]>(varName, varType);
+			case "booléen":
+				return new Variable<Boolean[]>(varName, varType);
+			case "chaîne de caractère":
+				return new Variable<String[]>(varName, varType);
+			case "réel":
+				return new Variable<Double[]>(varName, varType);
+			case "caractère":
+				return new Variable<Character[]>(varName, varType);
+
+			default:
+				return null;
+			}
+		}
+
+		
+		//variables simples
 		switch (varType) {
+
 		case "entier":
 			return new Variable<Integer>(varName, varType);
 		case "booléen":
