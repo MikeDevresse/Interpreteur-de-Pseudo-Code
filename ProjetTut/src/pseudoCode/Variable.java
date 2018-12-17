6f607f5ab17c5cd51a0ae6498ce9455816be384f
Loan <loan.alouache@etu.univ-lehavre.fr>
2018-12-17 09:58:44 +0100

    javadoc

--- ProjetTut/src/pseudoCode/Variable.java
+++ ProjetTut/src/pseudoCode/Variable.java
@@ -7,28 +7,54 @@ public class Variable<T> {
 	private T value;
 	private boolean constante;
 
+	/**
+	 * Crée une variable
+	 * @param name nom de la variable
+	 * @param type type de la variable
+	 * @param constante vrai si constante
+	 */
 	public Variable(String name, String type, boolean constante) {
 		this.name = name;
 		this.type = type;
 		this.constante = constante;
 	}
 
+	/**
+	 * Retourne la valeur de la variable
+	 * @return valeur
+	 */
 	public T getValue() {
 		return value;
 	}
 
+	/**
+	 * Retourne le nom de la variable
+	 * @return nom
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Retourne si la variable est une constante
+	 * @return vrai si constante
+	 */
 	public boolean isConstante() {
 		return this.constante;
 	}
 
-	public void setValue(T content) {
-		this.value = content;
+	/**
+	 * Défini la valeur de la constante
+	 * @param value valeur de la variable
+	 */
+	public void setValue(T value) {
+		this.value = value;
 	}
 
+	/**
+	 * Défini le nom de la variable
+	 * @param name nom de la variable
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
