9f8988b288038d233e3e935ba6d22f5b75e40bd5
Loan <alouache.loan@gmail.com>
2018-12-17 09:00:20 +0100

    correction merge

--- ProjetTut/src/pseudoCode/VariableFactory.java
+++ ProjetTut/src/pseudoCode/VariableFactory.java
@@ -1,31 +1,21 @@
 package pseudoCode;
 
-public class VariableFactory
-{
+public class VariableFactory {
 
-<<<<<<< HEAD
 	public static Variable createVariable(String expression) {
 		expression = expression.replace(" <", "<");
 		expression = expression.replace("- ", "-");
 		expression = expression.replace("d'", "de ");
-=======
-	public static Variable createVariable ( String expression )
-	{
-		expression = expression.replace( " <", "<" );
-		expression = expression.replace( "- ", "-" );
->>>>>>> branch 'master' of https://github.com/MikeDevresse/ProjetTutS3.git
 
-		String varName = expression.split( "<--" )[0];
-		String varType = expression.split( "<--" )[1];
+		String varName = expression.split("<--")[0];
+		String varType = expression.split("<--")[1];
 		varType = varType.toLowerCase();
 
-<<<<<<< HEAD
-		
-		//tableau de variables
+		// tableau de variables
 		String tabDelimiter = "tableau de ";
 		if (varType.indexOf(tabDelimiter) != -1) {
 			String tabType = varType.substring(varType.indexOf(tabDelimiter) + tabDelimiter.length());
-			
+
 			switch (tabType) {
 
 			case "entier":
@@ -44,8 +34,7 @@ public class VariableFactory
 			}
 		}
 
-		
-		//variables simples
+		// variables simples
 		switch (varType) {
 
 		case "entier":
@@ -58,23 +47,9 @@ public class VariableFactory
 			return new Variable<Double>(varName, varType);
 		case "caractère":
 			return new Variable<Character>(varName, varType);
-=======
-		switch ( varType )
-		{
-			case "entier" :
-				return new Variable<Integer>( varName, varType );
-			case "booléen" :
-				return new Variable<Boolean>( varName, varType );
-			case "chaîne de caractère" :
-				return new Variable<String>( varName, varType );
-			case "réel" :
-				return new Variable<Double>( varName, varType );
-			case "caractère" :
-				return new Variable<Character>( varName, varType );
->>>>>>> branch 'master' of https://github.com/MikeDevresse/ProjetTutS3.git
 
-			default :
-				return null;
+		default:
+			return null;
 		}
 	}
 
