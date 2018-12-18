26a4c78a80fc6a4b1254fb719983446077e42447
sl172427 <sl172427@localhost>
2018-12-18 16:12:57 +0100

    gestion windows

--- ProjetTut/src/ihmCui/Affichage.java
+++ ProjetTut/src/ihmCui/Affichage.java
@@ -2,6 +2,8 @@ package ihmCui;
 
 import java.util.HashMap;
 
+import org.fusesource.jansi.AnsiConsole;
+
 import iut.algo.Console;
 import iut.algo.CouleurConsole;
 import pseudoCode.Algorithme;
@@ -10,45 +12,50 @@ import pseudoCode.Variable;
 
 public class Affichage {
 	
-	public static final String ANSI_RESET  = "\u001B[0m";
-	public static final String ANSI_BLACK  = "\u001B[30m";
-	public static final String ANSI_RED    = "\u001B[31m";
-	public static final String ANSI_GREEN  = "\u001B[32m";
-	public static final String ANSI_YELLOW = "\u001B[33m";
-	public static final String ANSI_BLUE   = "\u001B[34m";
-	public static final String ANSI_PURPLE = "\u001B[35m";
-	public static final String ANSI_CYAN   = "\u001B[36m";
-	public static final String ANSI_WHITE  = "\u001B[37m";
+	private static final String ANSI_RESET  = "\u001B[0m";
+	private static final String ANSI_BLACK  = "\u001B[30m";
+	private static final String ANSI_RED    = "\u001B[31m";
+	private static final String ANSI_GREEN  = "\u001B[32m";
+	private static final String ANSI_YELLOW = "\u001B[33m";
+	private static final String ANSI_BLUE   = "\u001B[34m";
+	private static final String ANSI_PURPLE = "\u001B[35m";
+	private static final String ANSI_CYAN   = "\u001B[36m";
+	private static final String ANSI_WHITE  = "\u001B[37m";
 	
 	private String[] code;
-	private HashMap syntaxes;
+	private HashMap<String, String> syntaxes;
+	
+	private boolean OSWindows=false;
 	
 	/**
 	 * Constructeur
 	 * @param code pseudo-code
 	 */
 	public Affichage(String[] code) {
+		if(OSWindows)
+			AnsiConsole.systemInstall();
+		
 		this.code = code;
-		syntaxes = new HashMap();
+		syntaxes = new HashMap<String, String>();
 		
-		syntaxes.put("si",    ANSI_BLUE);
-		syntaxes.put("sinon", ANSI_BLUE);
-		syntaxes.put("fsi",   ANSI_BLUE);
-		syntaxes.put("alors", ANSI_BLUE);
+		syntaxes.put("si",         ANSI_BLUE);
+		syntaxes.put("sinon",      ANSI_BLUE);
+		syntaxes.put("fsi",        ANSI_BLUE);
+		syntaxes.put("alors",      ANSI_BLUE);
 		
-		syntaxes.put("fonction", ANSI_CYAN);
+		syntaxes.put("fonction",   ANSI_CYAN);
 		
-		syntaxes.put("entier", ANSI_GREEN);
-		syntaxes.put("double", ANSI_GREEN);
-		syntaxes.put("chaine", ANSI_GREEN);
-		syntaxes.put("booleen", ANSI_GREEN);
-		syntaxes.put("caractere", ANSI_GREEN);
+		syntaxes.put("entier",     ANSI_GREEN);
+		syntaxes.put("double",     ANSI_GREEN);
+		syntaxes.put("chaine",     ANSI_GREEN);
+		syntaxes.put("booleen",    ANSI_GREEN);
+		syntaxes.put("caractere",  ANSI_GREEN);
 		
 		syntaxes.put("ALGORITHME", ANSI_PURPLE);
-		syntaxes.put("variable", ANSI_PURPLE);
-		syntaxes.put("constante", ANSI_PURPLE);
-		syntaxes.put("DEBUT", ANSI_PURPLE);
-		syntaxes.put("FIN", ANSI_PURPLE);
+		syntaxes.put("variable",   ANSI_PURPLE);
+		syntaxes.put("constante",  ANSI_PURPLE);
+		syntaxes.put("DEBUT",      ANSI_PURPLE);
+		syntaxes.put("FIN",        ANSI_PURPLE);
 	}
 	
 	/**
