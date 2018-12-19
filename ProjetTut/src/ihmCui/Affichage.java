51da6bc97766641d77a4fb880b00af7fa514cb2f
sl172427 <sl172427@localhost>
2018-12-19 08:24:46 +0100

    Affichage windows

--- ProjetTut/src/ihmCui/Affichage.java
+++ ProjetTut/src/ihmCui/Affichage.java
@@ -41,8 +41,6 @@ public class Affichage {
 	 * @param code pseudo-code
 	 */
 	public Affichage(String[] code) {
-		if(OSWindows)
-			AnsiConsole.systemInstall();
 		
 		this.code = code;
 		syntaxes = new HashMap<String, String>();
@@ -127,8 +125,13 @@ public class Affichage {
 		}
 		
 		affichage += console(exec);
-		if(OSWindows) AnsiConsole.out.println(affichage);AnsiConsole.systemUninstall();
-		System.out.println(affichage);
+		if(OSWindows) { 
+			AnsiConsole.systemInstall();
+			AnsiConsole.out.println(affichage);
+			AnsiConsole.systemUninstall();
+		}
+		else
+			System.out.println(affichage);
 	}
 	
 	private String entete() {
