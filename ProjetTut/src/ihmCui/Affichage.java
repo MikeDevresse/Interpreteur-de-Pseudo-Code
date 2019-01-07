3491a9c57b81bbf6211b8e3823ee3094b0c70dfa
mikize <mike.devresse@gmail.com>
2019-01-07 10:31:25 +0100

    fix coloration

--- ProjetTut/src/ihmCui/Affichage.java
+++ ProjetTut/src/ihmCui/Affichage.java
@@ -243,7 +243,7 @@ public class Affichage {
 		copy = copy.replaceAll("(\".*\")", syntaxes.get("griffe") + "$1" + ANSI_BACK);
 
 		str = str.replaceAll("([é\\w]+[\\s]*)\\(", syntaxes.get("fonction") + "$1" + ANSI_BACK + "(");
-		str = str.replaceAll(".+:(.)*", syntaxes.get("type") + "$1" + ANSI_BACK );
+		str = str.replaceAll("(.+:[ ]*)([[ ]*[\\w\\[\\]\\']+]+)", "$1" + syntaxes.get("type") + "$2" + ANSI_BACK );
 
 		str = recupAnnotation(copy, str);
 
