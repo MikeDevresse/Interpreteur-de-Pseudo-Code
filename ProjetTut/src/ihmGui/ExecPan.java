e0249f9464745637e1047205d1e707e0902497bd
leon souffes <leon.souffes@gmail.com>
2019-01-09 08:47:59 +0100

    affichage commentaire gui

--- ProjetTut/src/ihmGui/ExecPan.java
+++ ProjetTut/src/ihmGui/ExecPan.java
@@ -1,8 +1,10 @@
 package ihmGui;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,26 +15,45 @@ import main.Controleur;
 import pseudoCode.Programme;
 
 public class ExecPan extends JPanel implements ActionListener {
+	private JPanel pan;
+	
 	private JScrollPane jsp;
 	private JLabel txt;
+	
+	private JScrollPane jspComm;
+	private JLabel txtComm;
 	
 	private JTextField console;
 	
 	private String exec;
 	private Programme prog;
 	
+	private HashMap<Integer,String> comms;
+	
 	public ExecPan(Programme prog) {
 		this.prog = prog;
 		console = new JTextField();
 		console.addActionListener( this );
 		
+		comms = Controleur.getControleur().getComms();
+		
+		pan = new JPanel();
+		
 		txt = new JLabel();
 		jsp = new JScrollPane(txt);
 		
+		txtComm = new JLabel();
+		jspComm = new JScrollPane(txtComm);
+		
 		paint();
 		
+		pan.setLayout(new GridLayout(1, 2));
+		
+		pan.add(jsp);
+		pan.add(jspComm);
+		
 		this.setLayout(new BorderLayout());
-		this.add(jsp, BorderLayout.CENTER);
+		this.add(pan, BorderLayout.CENTER);
 		this.add(console, BorderLayout.SOUTH);
 	}
 	
@@ -62,6 +83,30 @@ public class ExecPan extends JPanel implements ActionListener {
 		txt.setText(txt.getText() + "</body></html>");
 		try {Thread.sleep(50);}catch(Exception e){}
 		jsp.getVerticalScrollBar().setValue(jsp.getVerticalScrollBar().getMaximum());
+		
+		int ligneC = prog.getCurrent().getLigneCourrante();
+		String concat = "";
+		if(comms.containsKey(ligneC+1)) {
+			String strComm = comms.get(ligneC+1);
+			
+			for( int i=0; i<strComm.length(); i++ )
+			if(i%50==0) {
+				concat += "\n" + strComm.charAt(i);
+			}else {
+				concat += strComm.charAt(i);
+			}
+		}
+
+		txtComm.setText("<html><body style style=\"font-size: 15px\">");
+		
+		for(String str : concat.split("\n")) {
+			txtComm.setText(txtComm.getText()+"<p>"+str+"</p>");
+		}
+		
+		txtComm.setText(txtComm.getText()+"</body></html>");
+		
+		try {Thread.sleep(50);}catch(Exception e){}
+		jspComm.getVerticalScrollBar().setValue(jspComm.getVerticalScrollBar().getMaximum());
 	}
 
 	public void actionPerformed ( ActionEvent e )
